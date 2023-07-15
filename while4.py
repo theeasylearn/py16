5277fc5b05c47e01732d12dd58e4eae397586d3f
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-07-15 12:59:18 +0530

    list

---
+++ while4.py
@@ -0,0 +1,12 @@
+#write a program to findout factorial of given number 
+#number = 5
+# process = 5 x 4 x 3 x 2 x 1
+#output = 120 
+
+number = int(input("Enter number"))
+factorial = 1 
+
+while number>=1:
+    factorial = number * factorial #5
+    number = number - 1 #4
+print(factorial)
