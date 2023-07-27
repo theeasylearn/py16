9a70008ce7016318f696cc142c4ae595242448ff
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-07-27 13:04:44 +0530

    random number

---
+++ mathsdemo.py
@@ -0,0 +1,11 @@
+import math 
+
+number = float(input("Enter float number"))
+print(number)
+print("ceil of given value ",math.ceil(number))
+print("Floor of given value ",math.floor(number))
+print("truncated value of given value ",math.trunc(number))
+
+base = 2 
+exponent = 5 
+print("2 ^ 5 ",math.pow(base,exponent))
---
+++ randomdemo.py
@@ -0,0 +1,6 @@
+import random 
+number = random.random()
+print(f"any one random value between 0 and 1",number)
+print("random float number between 1 to 6",random.uniform(1,6))
+print("random integer number between 1 to 6",random.randint(1,6))
+print("random integer number between 10 to 1000 ",random.randrange(10,1000,10))
